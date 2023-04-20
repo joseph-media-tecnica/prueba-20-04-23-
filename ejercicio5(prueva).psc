a04f3b3c66910f14e95dbb816ac6b3fecaa9952f
joseph-media-tecnica <josephhidalgo2019@gmail.com>
2023-04-20 15:47:37 -0500

    pruebas

---
+++ ejercicio5(prueva).psc
@@ -0,0 +1,15 @@
+Algoritmo ejercicio5
+	definir tela,pulgada,resultado Como Real
+	
+	Escribir "Ingrese cuantos metros de tela requiere:";
+	Leer tela;
+	
+	pulgada=0.0254;
+	
+	resultado=tela/pulgada;
+	
+	Escribir "Necesitas"," ",resultado," ","pulgadas de tela";
+	
+	
+	
+FinAlgoritmo
---
+++ scrypt/script.js
@@ -0,0 +1,24 @@
+let num1,num2,suma,resta,multiplicacion,division;
+
+num1=5;
+num2=7;
+
+//suma
+
+suma=Number(num1+num2);
+console.log(`el resultado de la suma es:`+suma);
+
+//resta
+
+resta=Number(num1-num2);
+console.log(`el resultado de la resta es:`+resta);
+
+//multiplicacion
+
+multiplicacion=Number(num1*num2);
+console.log(`el resultado de la multiplicacion es:`+multiplicacion);
+
+//division
+
+division=Number(num1/num2);
+console.log(`el resultado de la division es:`+division);
